6d0a9748d1e40c3ddbe97d576aaaf93f55b1cf54
giotosi94 <gio_tosi@hotmail.it>
2026-06-16 20:33:52 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ToastProvider } from './components/Toast';
+import ErrorBoundary from './components/ErrorBoundary';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <ErrorBoundary>
+    <ToastProvider>
+      <App />
+    </ToastProvider>
+  </ErrorBoundary>
+);
