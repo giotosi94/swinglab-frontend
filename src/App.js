58806bdbf42973779cef76614b3cb704edf68b45
giotosi94 <gio_tosi@hotmail.it>
2026-09-21 16:46:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ function App() {
 
   const refreshData = async () => {
     setLoading(true);
-    const result = await api.fetchSectorsAndAssets(305);
+    const result = await api.fetchSectorsAndAssets(250);
     if (result) {
       setSectors(result.sectors);
       setAssets(result.assets);
