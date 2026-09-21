11bb25aa8d9720ce7ddf4a57f813289a97facb33
giotosi94 <gio_tosi@hotmail.it>
2026-09-21 16:11:24 +0200

    Change default limit in fetchSectorsAndAssets function

--- src/utils/api.js
+++ src/utils/api.js
@@ -38,7 +38,7 @@ export const runTrader = () => post('/api/data/autotrader/run');
 export const searchStock = (query) => get(`/api/data/search/${query.trim().toUpperCase()}`);
 export const fetchAllTickers = () => get('/api/data/tickers/list');
 
-export async function fetchSectorsAndAssets(limit = 350) {
+export async function fetchSectorsAndAssets(limit = 305) {
   try {
     const [sRes, aRes] = await Promise.all([
       fetch(`${API}/api/sectors`),
